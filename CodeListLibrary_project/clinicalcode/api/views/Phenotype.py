786720305d37f13ce0bfec8ec58024299693ac3b
CHI\Muhammad.Elmessary <muhammad.elmessary@swansea.ac.uk>
2021-09-07 21:10:31 +0100

    allow api search by forcing a specific brand

--- CodeListLibrary_project/clinicalcode/api/views/Phenotype.py
+++ CodeListLibrary_project/clinicalcode/api/views/Phenotype.py
@@ -553,14 +553,12 @@ def getPhenotypes(request, is_authenticated_user=True, pk=None):
         filter_cond += " AND COALESCE(validation_performed, FALSE) IS TRUE "
 
     # show phenotypes for a specific brand
+    force_brand = None
     if phenotype_brand != "":
         if Brand.objects.all().filter(name__iexact = phenotype_brand.strip()).exists():
-            current_brand = Brand.objects.all().filter(name__iexact = phenotype_brand.strip())
-            group_list = list(current_brand.values_list('groups', flat=True))
-            filter_cond += " AND group_id IN("+ ', '.join(map(str, group_list)) +") "
-        else:
-            # brand name not found
-            filter_cond += " AND group_id IN(-1) "
+            current_brand = Brand.objects.get(name__iexact = phenotype_brand.strip())
+            force_brand = current_brand.name
+
        
     phenotypes_srch = get_visible_live_or_published_phenotype_versions(request
                                                 , get_live_and_or_published_ver = get_live_and_or_published_ver 
@@ -569,6 +567,7 @@ def getPhenotypes(request, is_authenticated_user=True, pk=None):
                                                 , exclude_deleted = exclude_deleted
                                                 , filter_cond = filter_cond
                                                 , show_top_version_only = show_top_version_only
+                                                , force_brand = force_brand
                                                 )
          
 
