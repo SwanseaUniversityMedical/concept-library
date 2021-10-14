315928d19e28258e0bfc8c3b1ac26a979e2b8ddb
Arthur <zinnurov_2012@mail.ru>
2021-10-14 15:19:43 +0100

    Added pop up and settings functionality with reject

--- CodeListLibrary_project/cll/static/cookielaw/js/cookiealert.js
+++ CodeListLibrary_project/cll/static/cookielaw/js/cookiealert.js
@@ -1,12 +1,14 @@
-
 (function () {
     "use strict";
 
     var cookieAlert = document.querySelector(".cookiealert");
     var acceptCookies = document.querySelector(".acceptcookies");
+    var rejectCookies = document.querySelector(".rejectcookies");
+    var settingsCookie = document.querySelector(".preferencecookies");
+
 
     if (!cookieAlert) {
-       return;
+        return;
     }
 
     cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
@@ -15,6 +17,16 @@
     if (!getCookie("acceptCookies")) {
         cookieAlert.classList.add("show");
     }
+
+    if (getCookie("rejectCookies")) {
+        cookieAlert.classList.remove("show");
+    }
+
+    if (getCookie("cookieSettings")) {
+        cookieAlert.classList.remove("show");
+
+    }
+
 
     function getCookie(cname) {
         var name = cname + "=";
@@ -32,7 +44,7 @@
         return "";
     }
 
-    
+
     function setCookie(cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -40,30 +52,45 @@
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
+
     // When clicking on the agree button, create a 1 year
     // cookie to remember user's choice and close the banner
     acceptCookies.addEventListener("click", function () {
         setCookie("acceptCookies", true, 365);
-         cookieAlert.classList.remove("show");
+        cookieAlert.classList.remove("show");
+        // document.getElementById("accept_button").value=1;
+
 
         // dispatch the accept event
         window.dispatchEvent(new Event("cookieAlertAccept"))
 
     });
 
-   // var ele = document.getElementById('link_privacy')
+
+    rejectCookies.addEventListener("click", function () {
+        cookieAlert.classList.remove("show");
+        //document.getElementById("reject_button").value=1;
+        setCookie("rejectCookies", false, 365);
+        window.dispatchEvent(new Event("rejectCookiesAlert"))
+
+    });
+
+    settingsCookie.addEventListener("click", function () {
+        cookieAlert.classList.remove("show");
+
+         window.dispatchEvent(new Event("preferencecookiesAlert"))
+    });
+
+
+    // var ele = document.getElementById('link_privacy')
     //ele.addEventListener('click',function(){
-      //   setCookie("acceptCookies", true, 365);
-        // cookieAlert.classList.remove("show");
+    //   setCookie("acceptCookies", true, 365);
+    // cookieAlert.classList.remove("show");
 
-        // dispatch the accept event
-        //window.dispatchEvent(new Event("cookieAlertAccept"))
-   // });
-
-
+    // dispatch the accept event
+    //window.dispatchEvent(new Event("cookieAlertAccept"))
+    // });
 
 
-
-
-    
 })();
+
