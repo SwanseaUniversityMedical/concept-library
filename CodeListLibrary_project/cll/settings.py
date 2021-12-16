ed3ce3c19a7de0e0bdb851162ccbeb238f02fd15
CHI\Muhammad.Elmessary <muhammad.elmessary@swansea.ac.uk>
2021-12-16 17:01:29 +0000

    contact-us pg

--- CodeListLibrary_project/cll/settings.py
+++ CodeListLibrary_project/cll/settings.py
@@ -138,8 +138,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 CLINICALCODE_SESSION_ID = 'concept'
 
 #===========================================================================
@@ -416,7 +414,8 @@ if IS_DEMO:  # Demo server
     DEV_PRODUCTION = "<i class='glyphicon glyphicon-cog'  aria-hidden='true'> </i> DEMO SITE <i class='glyphicon glyphicon-cog'  aria-hidden='true'> </i>"
  
 
-
+##EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email, contact us page
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
