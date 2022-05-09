a201b02709ca4953732127fe0ebec4c167ce22b5
elmessary <muhammad.elmessary@swansea.ac.uk>
2022-05-09 20:31:49 +0100

    dynamic robot.txt / sitemap  p1

--- CodeListLibrary_project/clinicalcode/api/urls.py
+++ CodeListLibrary_project/clinicalcode/api/urls.py
@@ -71,8 +71,12 @@ schema_view = get_schema_view(
 urlpatterns += [
     url(r'^swagger(?P<format>\.json|\.yaml)/$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.IS_DEVELOPMENT_PC:
+    urlpatterns += [
+        url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]    
 ###########################################################################
 
 
