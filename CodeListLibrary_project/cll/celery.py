2e4b7a809b205ea25b8e5fda623060404803518e
Arthur <zinnurov_2012@mail.ru>
2022-06-06 13:56:00 +0100

    Added datasource sync celery

--- CodeListLibrary_project/cll/celery.py
+++ CodeListLibrary_project/cll/celery.py
@@ -14,19 +14,23 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.beat_schedule = {
     'send_mail': {
         'task': 'clinicalcode.tasks.send_scheduled_email',
-        'schedule': crontab(minute=5) if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
+        'schedule': crontab(minute='*/5') if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
     },
     'send_message_test': {
         'task': 'clinicalcode.tasks.send_message_test',
-        'schedule':crontab(minute=5) if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
+        'schedule':crontab(minute='*/5') if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
 
     }, 'celery_run_statistics': {
         'task': 'clinicalcode.views.Admin.run_celery_statistics',
-        'schedule': crontab(minute=5) if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
+        'schedule': crontab(minute='*/5') if settings.IS_DEVELOPMENT_PC else crontab(minute=0,hour='9,18')
     },
     'celery_run_collections': {
         'task': 'clinicalcode.views.Admin.run_celery_collections',
-        'schedule': crontab(minute=5) if settings.IS_DEVELOPMENT_PC else crontab(minute=58,hour=12)
+        'schedule': crontab(minute='*/5') if settings.IS_DEVELOPMENT_PC else crontab(minute=0,hour='9,18')
+
+    },'celery_run_data_sync':{
+        'task': 'clinicalcode.views.Admin.run_celery_datasource',
+        'schedule': crontab(minute='*/5') if settings.IS_DEVELOPMENT_PC else crontab(minute=0, hour='9,18')
     }
 
 }
