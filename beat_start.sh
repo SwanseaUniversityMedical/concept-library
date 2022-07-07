102fcc4027580f599da1274d8376e85149c9ec82
Arthur <zinnurov_2012@mail.ru>
2022-07-07 16:00:16 +0100

    Added config for celery/celery beat to discard previous tasks when deploy

--- beat_start.sh
+++ beat_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /var/www/concept_lib_sites/v1 && virtualenv  cllvirenv_v1 && . cllvirenv_v1/bin/activate && cd /var/www/concept_lib_sites/v1/CodeListLibrary_project &&  celery -A cll beat -l INFO
+cd /var/www/concept_lib_sites/v1 && virtualenv  cllvirenv_v1 && . cllvirenv_v1/bin/activate && cd /var/www/concept_lib_sites/v1/CodeListLibrary_project &&  celery -A cll beat -l INFO --max-interval 300
 
 
--- worker_start.sh
+++ worker_start.sh
@@ -3,6 +3,6 @@ cd /var/www/concept_lib_sites/v1 &&
 virtualenv  cllvirenv_v1 &&
 . cllvirenv_v1/bin/activate &&
 cd /var/www/concept_lib_sites/v1/CodeListLibrary_project &&
-celery -A cll worker -l INFO
+celery -A cll worker -l INFO --purge
 
 
