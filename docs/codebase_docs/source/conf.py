ca426b4d115da2b6847071bf6b15e198851d008d
Alex Coldea <a.i.coldea@swansea.ac.uk>
2022-07-13 07:06:14 +0100

    migrations

--- docs/codebase_docs/source/conf.py
+++ docs/codebase_docs/source/conf.py
@@ -55,7 +55,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['**/clinicalcode/migrations', '_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['**/clinicalcode/migrations', 'clinicalcode/migrations', 'clinicalcode/migrations*', 'clinicalcode/migrations/*','_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
