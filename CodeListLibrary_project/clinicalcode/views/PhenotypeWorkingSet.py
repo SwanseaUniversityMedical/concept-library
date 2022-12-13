4b8dcf82777e479c101790d90532a36f4d0b7fd9
Artur <zinnurov_2012@mail.ru>
2022-12-13 16:43:06 +0000

    Adding messages to operations in workingset

--- CodeListLibrary_project/clinicalcode/views/PhenotypeWorkingSet.py
+++ CodeListLibrary_project/clinicalcode/views/PhenotypeWorkingSet.py
@@ -689,7 +689,7 @@ def WorkingsetDetail_combined(request, pk, workingset_history_id=None):
         'publish_date': publish_date,
         'is_latest_version': is_latest_version,
         'is_latest_pending_version': is_latest_pending_version,
-        'current_phenotype_history_id': int(workingset_history_id),
+        'current_workingset_history_id': int(workingset_history_id),
         'page_canonical_path': get_canonical_path_by_brand(request, PhenotypeWorkingset, pk, workingset_history_id),
         'q': db_utils.get_q_highlight(request, request.session.get('ph_workingset_search', '')),
         'force_highlight_result': ['0', '1'][db_utils.is_referred_from_search_page(request)]
@@ -756,6 +756,104 @@ def get_history_table_data(request, pk):
 
     return historical_versions
 
+
+def form_validation(self, request, data, workingset_history_id, pk, workingset,checks):
+    data['form_is_valid'] = True
+    data['latest_history_ID'] = workingset_history_id  # workingset.history.latest().pk
+
+    # update history list
+    data['html_history_list'] = render_to_string(
+        'clinicalcode/workingset/partial_history_list.html',
+        {
+            'history': get_history_table_data(request, pk),  # workingset.history.all(),
+            'current_workingset_history_id': int(workingset_history_id),  # workingset.history.latest().pk,
+            'published_historical_ids':
+                list(PublishedWorkingset.objects.filter(workingset_id=pk, approval_status=2).values_list('workingset_history_id', flat=True))
+
+        },
+        request=self.request)
+
+    data['message'] = send_message(pk, workingset_history_id, data, workingset,checks)['message']
+    return data
+
+
+def send_message(self, pk, workingset_history_id, data, workingset,checks):
+    if approval_status == 2:
+        data['message'] = render_to_string('clinicalcode/workingset/published.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        self.send_email_decision(workingset, approval_status)
+        return data
+
+    elif len(PublishedWorkingset.objects.filter(workingset=PhenotypeWorkingset.objects.get(pk=pk).id, approval_status=2)) > 0 and not approval_status == 3:
+        data['message'] = render_to_string('clinicalcode/workingset/published.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        send_email_decision_workingset(workingset, approval_status)
+
+        return data
+
+
+    elif approval_status == 1:
+        data['message'] = render_to_string('clinicalcode/workingset/published.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        send_email_decision_workingset(workingset, approval_status)
+        return data
+
+    elif approval_status == 3:
+        data['message'] = render_to_string('clinicalcode/phenotypeworkingset/declined.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        send_email_decision_workingset(workingset, approval_status)
+        return data
+
+    elif approval_status is None and is_moderator:
+        data['message'] = render_to_string('clinicalcode/workingset/published.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        return data
+
+    elif approval_status is None:
+        data['message'] = render_to_string('clinicalcode/workingset/approve.html',
+                                           {
+                                               'id': pk,
+                                               'workingset_history_id': workingset_history_id
+                                           },
+                                           self.request)
+        return data
+
+
+def send_email_decision_workingset(workingset, approved):
+    if approved == 1:
+        db_utils.send_review_email(workingset,
+                                   "Published",
+                                   "Workingset has been successfully approved and published on the website")
+
+    elif approved == 2:
+        # This line for the case when user want to get notification of same workingset id but different version
+        db_utils.send_review_email(workingset,
+                                   "Published",
+                                   "Workingset has been successfully approved and published on the website")
+    elif approved == 3:
+        db_utils.send_review_email(workingset,
+                                   "Rejected",
+                                   "Workingset has been rejected by the moderator. Please consider update changes and try again")
 
 class WorkingSetPublish(LoginRequiredMixin, HasAccessToEditPhenotypeWorkingsetCheckMixin, TemplateResponseMixin, View):
     '''
