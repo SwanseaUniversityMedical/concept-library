99efe3f953a699a691ef0a879ed3dcfd808fe875
Arthur <zinnurov_2012@mail.ru>
2023-02-24 16:08:33 +0000

    Adding general check to be published

--- CodeListLibrary_project/clinicalcode/view_utils/utils_ge_validator.py
+++ CodeListLibrary_project/clinicalcode/view_utils/utils_ge_validator.py
@@ -1,4 +1,6 @@
-from clinicalcode import urls_generic_entity
+from clinicalcode import generic_entity_db_utils
+from clinicalcode.models.GenericEntity import GenericEntity
+from clinicalcode.models.PublishedGenericEntity import PublishedGenericEntity
 
 
 def form_validation(request, data, entity_history_id, pk,entity,checks):
@@ -22,7 +24,7 @@ def form_validation(request, data, entity_history_id, pk,entity,checks):
             'history': get_history_table_data(request, pk),  # entity.history.all(),
             'current_entity_history_id': int(entity_history_id),  # entity.history.latest().pk,
             'published_historical_ids':
-                list(PublishedWorkingset.objects.filter(entity_id=pk, approval_status=2).values_list('entity_history_id', flat=True))
+                list(PublishedGenericEntity.objects.filter(entity_id=pk, approval_status=2).values_list('entity_history_id', flat=True))
         },
         request=request)
     #send email message state and client side message
@@ -48,7 +50,7 @@ def send_message( pk, data, entity,entity_history_id,checks):
         return data
 
     #publish message if not declined
-    elif len(PublishedWorkingset.objects.filter(workingset=PhenotypeWorkingset.objects.get(pk=pk).id, approval_status=2)) > 0 and not data['approval_status'] == 3:
+    elif len(PublishedGenericEntity.objects.filter(workingset=PhenotypeWorkingset.objects.get(pk=pk).id, approval_status=2)) > 0 and not data['approval_status'] == 3:
         data['message'] = """The workingset version has been successfully published.
                                  <a href='{url}' class="alert-link">(WORKINGSET ID: {pk}, VERSION ID:{history} )</a>""".format(url=reverse('phenotypeworkingset_history_detail', args=(pk,workingset_history_id)),
                                                                                                          pk=pk,history=workingset_history_id)
@@ -86,6 +88,106 @@ def send_message( pk, data, entity,entity_history_id,checks):
         return data
 
 
+def checkEntityTocheck(request,pk,history_id):
+    entity = GenericEntity.objects.get(id=pk,history_id=history_id)
+
+    if entity.layout == 1:
+        return checkPhenotypeTobePublished(request,pk,history_id)
+    elif entity.layout == 2:
+        return checkWorkingsetTobePublished(request,pk,history_id)
+    elif entity.layout == 3:
+        return checkConcpetTobePublished(request,pk,history_id)
+
+
+def checkWorkingsetTobePublished(request,pk,workingset_history_id):
+    '''
+        Allow to publish if:
+        - workingset is not deleted
+        - user is an owner
+        - Workingset contains codes
+        - all conceots are published
+        @param request: user request object
+        @param pk: workingset id
+        @param workingset_history_id: historical id of workingset
+        @return: dictionary containing all conditions to publish
+    '''
+    allow_to_publish = True
+    workingset_is_deleted = False
+    is_owner = True
+    is_moderator = False
+    is_latest_pending_version = False
+
+    if (GenericEntity.objects.get(id=pk).is_deleted == True):
+        allow_to_publish = False
+        workingset_is_deleted = True
+
+    #check if user is not owner
+    if (GenericEntity.objects.filter(Q(id=pk), Q(owner=request.user)).count() == 0):
+        allow_to_publish = False
+        is_owner = False
+
+    if (request.user.groups.filter(name="Moderators").exists()):
+        allow_to_publish = True
+        is_moderator = True
+
+    #check if either moderator or owner
+    if (request.user.groups.filter(name="Moderators").exists()
+            and not (GenericEntity.objects.filter(Q(id=pk), Q(owner=request.user)).count() == 0)):
+        allow_to_publish = True
+        is_owner = True
+        is_moderator = True
+
+    #check if current version of ws is the latest version to approve
+    if len(PublishedGenericEntity.objects.filter(workingset_id=pk, workingset_history_id=workingset_history_id, approval_status=1)) > 0:
+        is_latest_pending_version = True
+
+
+    workingset_ver = GenericEntity.history.get(id=pk, history_id=workingset_history_id)
+    is_published = checkIfPublished(GenericEntity, pk, workingset_history_id)
+    approval_status = get_publish_approval_status(GenericEntity, pk, workingset_history_id)
+    is_lastapproved = len(PublishedGenericEntity.objects.filter(workingset=GenericEntity.objects.get(pk=pk).id, approval_status=2)) > 0
+    other_pending = len(PublishedGenericEntity.objects.filter(workingset=GenericEntity.objects.get(pk=pk).id, approval_status=1)) > 0
+
+    # get historical version by querying SQL command from DB
+    workingset = getHistoryGenericEntity(workingset_history_id,
+                                                                   highlight_result=[False, True][
+                                                                       generic_entity_db_utils.is_referred_from_search_page(request)],
+                                                                   q_highlight=generic_entity_db_utils.get_q_highlight(request,
+                                                                                                        request.session.get(
+                                                                                                            'ph_workingset_search',
+                                                                                                            ''))
+                                                                   )
+    has_child_phenenotypes, isOK, all_not_deleted, all_are_published, is_allowed_view_children, errors = \
+        checkAllChildData4Publish_Historical(request,workingset_history_id)
+
+    if not isOK:
+        allow_to_publish = False
+
+    #check if table is not empty
+    workingset_has_data = len(GenericEntity.history.get(id=pk, history_id=workingset_history_id).phenotypes_concepts_data) > 0
+    if not workingset_has_data:
+        allow_to_publish = False
+
+
+    checks = {
+        'workingset': workingset,
+        'name': workingset_ver.name,
+        'errors':errors,
+        'allowed_to_publish':allow_to_publish,
+        'workingset_is_deleted':workingset_is_deleted,
+        'is_owner':is_owner,
+        'is_moderator':is_moderator,
+        'approval_status': approval_status,
+        'is_lastapproved': is_lastapproved,
+        'other_pending':other_pending,
+        'workingset_has_data':workingset_has_data,
+        'is_published': is_published,
+        'is_latest_pending_version':is_latest_pending_version,
+        'is_allowed_view_children':is_allowed_view_children,#to see if child phenotypes of ws is not deleted/not published etc
+        'all_are_published':all_are_published,
+        'all_not_deleted':all_not_deleted
+    }
+    return checks
 
 def send_email_decision_entity(entity, approved):
     """
@@ -95,16 +197,16 @@ def send_email_decision_entity(entity, approved):
     """
     
     if approved == 1:
-        urls_generic_entity.send_review_email(entity,
+        generic_entity_db_utils.send_review_email(entity,
                                    "Published",
                                    "Workingset has been successfully approved and published on the website")
 
     elif approved == 2:
         # This line for the case when user want to get notification of same workingset id but different version
-        urls_generic_entity.send_review_email(entity,
+        generic_entity_db_utils.send_review_email(entity,
                                    "Published",
                                    "Workingset has been successfully approved and published on the website")
     elif approved == 3:
-        urls_generic_entity.send_review_email(entity,
+        generic_entity_db_utils.send_review_email(entity,
                                    "Rejected",
                                    "Workingset has been rejected by the moderator. Please consider update changes and try again")
