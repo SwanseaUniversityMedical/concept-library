99413fc60fb356555894b9cade95fba5074bb776
JackScanlon <jackascanlon@hotmail.co.uk>
2023-03-05 10:13:14 +0000

    Fixed indentation

--- CodeListLibrary_project/clinicalcode/forms/TemplateForm.py
+++ CodeListLibrary_project/clinicalcode/forms/TemplateForm.py
@@ -2,14 +2,13 @@ from django import forms
 from ..models.Template import Template
 
 class TemplateAdminForm(forms.ModelForm):
-  '''
-    TemplateAdminForm
-      @desc adds a checkbox item to the admin form to forcefully update the JSONB field order
-            otherwise, ignores order update(s)
-
-  '''
+    '''
+        TemplateAdminForm
+            @desc adds a checkbox item to the admin form to forcefully update the JSONB field order
+                  otherwise, ignores order update(s)
+    '''
     update_order = forms.BooleanField(required=False)
 
     class Meta:
-      model = Template
-      fields = '__all__'
+        model = Template
+        fields = '__all__'
