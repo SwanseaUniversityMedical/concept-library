8b2fec6a3f2d92df5aa475a1c1d0d17562b585a7
elmessary <muhammad.elmessary@swansea.ac.uk>
2023-03-14 17:06:14 +0000

    api_remove_data

--- CodeListLibrary_project/clinicalcode/views/adminTemp.py
+++ CodeListLibrary_project/clinicalcode/views/adminTemp.py
@@ -45,12 +45,13 @@ def api_remove_data(request):
         raise PermissionDenied
 
     if settings.CLL_READ_ONLY:
+    if settings.CLL_READ_ONLY: 
         raise PermissionDenied
 
     if request.method == 'GET':
         if not settings.CLL_READ_ONLY and (settings.IS_DEMO or settings.IS_DEVELOPMENT_PC):
             return render(request, 'clinicalcode/adminTemp/adminTemp.html', 
-                          {'url': reverse('json_adjust_phenotype'),
+                          {'url': reverse('api_remove_data'),
                            'action_title': 'Delete API Data'
                         }
                         )
