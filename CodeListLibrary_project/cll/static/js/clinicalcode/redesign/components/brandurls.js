2e8e13f3f975d6143881c1b0b3e1b90df175ffc9
Arthur <zinnurov_2012@mail.ru>
2023-04-04 10:33:09 +0100

    more fixes to js

--- CodeListLibrary_project/cll/static/js/clinicalcode/redesign/components/brandurls.js
+++ CodeListLibrary_project/cll/static/js/clinicalcode/redesign/components/brandurls.js
@@ -1,4 +1,4 @@
-const brandUrlsgen = (all_brands, prod, currentBrand, classId) => {
+function brandUrlsgen(all_brands, prod,currentBrand) {
   var lTrimRegex = new RegExp("^/");
   var lTrim = function (input) {
     return input.replace(lTrimRegex, "");
@@ -9,67 +9,64 @@ const brandUrlsgen = (all_brands, prod, currentBrand, classId) => {
     all_brands.push(brand.toUpperCase());
   }
 
-  const linkElement = document.querySelector("#"+classId);
 
-  linkElement.addEventListener("click", (e) => {
-    var path = window.location.pathname;
-    path = lTrim(path);
-    old_root = path.split("/")[0];
+  var path = window.location.pathname;
+  path = lTrim(path);
+  old_root = path.split("/")[0];
 
-    new_root = '';
-    if (linkElement.getAttribute('value') != '') {
-      new_root = "/" + linkElement.getAttribute('value');
-    }
+ 
 
-    indx = 1;
-    if (all_brands.indexOf(old_root.toUpperCase()) == -1) {
-      indx = 0;
-    }
 
-    if (prod!="False") {
-      if (
-        window.location.href
-          .toLowerCase()
-          .includes("phenotypes.healthdatagateway".toLowerCase())
-      ) {
-        if (new_root == "/HDRUK") {
-          // do nothing
-        } else {
-          window.location.href =
-            "https://conceptlibrary.saildatabank.com" +
-            new_root +
-            "/" +
-            path.split("/").slice(indx).join("/");
-            return window.location.href;
-        }
-        
+  new_root = "";
+  console.log(currentBrand);
+  if (currentBrand != "") {
+    new_root = "/" + currentBrand;
+  }
+
+  indx = 1;
+  if (all_brands.indexOf(old_root.toUpperCase()) == -1) {
+    indx = 0;
+  }
+
+  if (prod != "False") {
+    if (
+      window.location.href
+        .toLowerCase()
+        .includes("phenotypes.healthdatagateway".toLowerCase())
+    ) {
+      if (new_root == "/HDRUK") {
+        // do nothing
       } else {
-        if (new_root == "/HDRUK") {
-          new_root = "";
-          window.location.href =
-            "https://phenotypes.healthdatagateway.org" +
-            new_root +
-            "/" +
-            path.split("/").slice(indx).join("/");
-            return window.location.href;
-        } else {
-          window.location.href =
-            "https://conceptlibrary.saildatabank.com" +
-            new_root +
-            "/" +
-            path.split("/").slice(indx).join("/");
-            return window.location.href;
-        }
-       
+        window.location.href =
+          "https://conceptlibrary.saildatabank.com" +
+          new_root +
+          "/" +
+          path.split("/").slice(indx).join("/");
+        return window.location.href;
       }
     } else {
-      window.location.href =
-        document.location.origin +
-        new_root +
-        "/" +
-        path.split("/").slice(indx).join("/");
-
-      return window.location.href;
+      if (new_root == "/HDRUK") {
+        new_root = "";
+        window.location.href =
+          "https://phenotypes.healthdatagateway.org" +
+          new_root +
+          "/" +
+          path.split("/").slice(indx).join("/");
+        return window.location.href;
+      } else {
+        window.location.href =
+          "https://conceptlibrary.saildatabank.com" +
+          new_root +
+          "/" +
+          path.split("/").slice(indx).join("/");
+        return window.location.href;
+      }
     }
-  });
-};
+  } else {
+    window.location.href =
+      document.location.origin +
+      new_root +
+      "/" +
+      path.split("/").slice(indx).join("/");
+  }
+}
