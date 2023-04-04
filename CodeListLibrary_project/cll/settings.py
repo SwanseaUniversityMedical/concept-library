7c6d47107f5e4b1391507b13391e5c861fbd27a6
Arthur <zinnurov_2012@mail.ru>
2023-04-04 17:07:46 +0100

    Adding svg module

--- CodeListLibrary_project/cll/settings.py
+++ CodeListLibrary_project/cll/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cll/static'),
 ]
 
+
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
@@ -209,6 +211,7 @@ INSTALLED_APPS = INSTALLED_APPS + [
     #'rest_framework_swagger',
     'drf_yasg',
     'django.contrib.sitemaps',
+    'svg',
     # SCSS
     'sass_processor',
 ]
