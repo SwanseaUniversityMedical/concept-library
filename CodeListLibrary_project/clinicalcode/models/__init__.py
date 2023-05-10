bed29e339e783c76479772454e16763b0949ae1e
elmessary <muhammad.elmessary@swansea.ac.uk>
2023-05-10 19:23:26 +0100

    ignore EMIS, Vision cs for now

--- CodeListLibrary_project/clinicalcode/models/__init__.py
+++ CodeListLibrary_project/clinicalcode/models/__init__.py
@@ -39,5 +39,7 @@ from .GPRD_PRODUCT_CODES import GPRD_PRODUCT_CODES
 from .Template import Template
 from .GenericEntity import GenericEntity
 from .PublishedGenericEntity import PublishedGenericEntity
-from .EMIS_CODES import EMIS_CODES
-from .VISION_CODES import VISION_CODES
+
+# need to restore EMIS/Vision when deploy. to prod.
+#from .EMIS_CODES import EMIS_CODES
+#from .VISION_CODES import VISION_CODES
