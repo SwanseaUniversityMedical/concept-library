5f3442d06412bbf1f2fe738107444945a53ce440
test <test@test.copm>
2023-08-01 17:08:14 +0100

    adding redirect to js file

--- CodeListLibrary_project/cll/static/js/clinicalcode/redesign/forms/clinical/entityPublish.js
+++ CodeListLibrary_project/cll/static/js/clinicalcode/redesign/forms/clinical/entityPublish.js
@@ -1,7 +1,8 @@
 class PublishModal {
-  constructor(publish_url, decline_url) {
+  constructor(publish_url, decline_url,redirect_url) {
     this.publish_url = publish_url;
     this.decline_url = decline_url;
+    this.redirect_url = redirect_url;
     this.button = document.querySelector("#publish2");
     this.button.addEventListener("click", this.handleButtonClick.bind(this));
   }
@@ -68,8 +69,10 @@ class PublishModal {
           const name = result.name;
           if (name == "Decline") {
             await this.postData(data, this.decline_url);
+            window.location.href = this.redirect_url
           } else {
             await this.postData(data, this.publish_url);
+            window.location.href = this.redirect_url
           }
         })
         .catch((result) => {
@@ -185,8 +188,10 @@ class PublishModal {
 domReady.finally(() => {
   const url_publish = document.querySelector('data[id="publish-url"]');
   const url_decline = document.querySelector('data[id="decline-url"]');
+  const redirect_url = document.querySelector('data[id="redirect-url"]');
   window.entityForm = new PublishModal(
     url_publish.innerHTML,
-    url_decline.innerHTML
+    url_decline.innerHTML,
+    redirect_url.innerHTML
   );
 });
