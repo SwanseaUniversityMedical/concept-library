fe00192a079ef289da526513cbc4e5c3659b22d2
JackScanlon <jackascanlon@hotmail.co.uk>
2023-09-06 13:14:39 +0100

    Move legacy tests, small fixes

--- CodeListLibrary_project/cll/static/js/clinicalcode/cookies/cookie-settings.js
+++ CodeListLibrary_project/cll/static/js/clinicalcode/cookies/cookie-settings.js
@@ -1,6 +1,4 @@
-
 const cookieSettings = (privacyurl) => {
-    
   ModalFactory.create({
     id: 'cookie-dialog',
     title: 'Privacy Settings',
@@ -24,32 +22,28 @@ const cookieSettings = (privacyurl) => {
       return console.error(result);
     }
   });
-
-
 }
 
 const generateContent = (url) => {
-    
-    let maindescription = `<p>We use cookies and similar technologies that are necessary to operate the website. Additional cookies
-    are only used with your consent. We use the additional cookies to perform analyses of website usage
-    and to check marketing measures for their efficiency. These analyses are carried out to provide you
-    with a better user experience on the website. You are free to give, deny, or withdraw your consent
-    at any time by using the "cookie settings" link at the bottom of each page. You can consent to our
-    use of cookies by clicking "Save selection". For more information about what information is collected and how
-    it is shared with our partners, please read our <a
-    href=${url}>Privacy and cookie policy</a>.</a>
-    </p>
+  let maindescription = `<p>We use cookies and similar technologies that are necessary to operate the website. Additional cookies
+  are only used with your consent. We use the additional cookies to perform analyses of website usage
+  and to check marketing measures for their efficiency. These analyses are carried out to provide you
+  with a better user experience on the website. You are free to give, deny, or withdraw your consent
+  at any time by using the "cookie settings" link at the bottom of each page. You can consent to our
+  use of cookies by clicking "Save selection". For more information about what information is collected and how
+  it is shared with our partners, please read our <a
+  href=${url}>Privacy and cookie policy</a>.</a>
+  </p>
 
-    <div class="checkbox-item-container min-size">
-    <input id="neccesary-cookies" type="checkbox" disabled checked class="checkbox-input" data-value="1" data-name="must-cookies"/>
-    <label for="neccesary-cookies">Necesary cookies</label>
-    </div>
+  <div class="checkbox-item-container min-size">
+  <input id="neccesary-cookies" type="checkbox" disabled checked class="checkbox-input" data-value="1" data-name="must-cookies"/>
+  <label for="neccesary-cookies">Necesary cookies</label>
+  </div>
 
-    <div class="checkbox-item-container min-size">
-    <input id="stats-check"  type="checkbox" class="checkbox-input" data-value="0" data-name="analytics"/>
-    <label for="stats-check">Analytics</label.
-    </div>
-    <p>Monitoring website usage and optimizing the user experience.</p>`
-    
-    return maindescription;
+  <div class="checkbox-item-container min-size">
+  <input id="stats-check"  type="checkbox" class="checkbox-input" data-value="0" data-name="analytics"/>
+  <label for="stats-check">Analytics</label.
+  </div>
+  <p>Monitoring website usage and optimizing the user experience.</p>`
+  return maindescription;
 }
--- CodeListLibrary_project/cll/static/js/clinicalcode/cookies/generateGtag.js
+++ CodeListLibrary_project/cll/static/js/clinicalcode/cookies/generateGtag.js
@@ -4,21 +4,19 @@ const generateTag = (type,configId, parameters) => {
   function gtag() {
     dataLayer.push(arguments);
   }
-  gtag("js", new Date());
 
+  gtag("js", new Date());
   gtag("consent", type , parameters);
   gtag("config", configId);
   console.log("gtag", type, configId, parameters);
 };
 
 const removeGATags = () => {
-    const gaScripts = Array.from(document.querySelectorAll("script[src*='google-analytics.com']"));
-  
-    gaScripts.forEach(gaScript => {
-      gaScript.remove();
-    });
-  };
-  
-  window.removeGATags = removeGATags;
+  const gaScripts = Array.from(document.querySelectorAll("script[src*='google-analytics.com']"));
+  gaScripts.forEach(gaScript => {
+    gaScript.remove();
+  });
+};
 
+window.removeGATags = removeGATags;
   
