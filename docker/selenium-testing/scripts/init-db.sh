670ecaee9849f2546b85aefef134192fb51f2a0b
Arthur <zinnurov_2012@mail.ru>
2023-12-11 18:34:57 +0000

    some rearange of user restore

--- docker/selenium-testing/scripts/init-db.sh
+++ docker/selenium-testing/scripts/init-db.sh
@@ -7,9 +7,11 @@ echo "===================================="
 BACKUP_FILE=$(find /docker-entrypoint-initdb.d/db/ -name '*.backup'| head -1)
 if [ ! -z $BACKUP_FILE ] && [ -e $BACKUP_FILE ]; then 
   echo "[!>] Found backup, restoring from local"
-  /usr/bin/psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $UNIT_TEST_DB_USER WITH PASSWORD '$UNIT_TEST_DB_PASSWORD'; ALTER USER $UNIT_TEST_DB_USER CREATEDB;"
   echo "[!>] Restoring database from local .backup"
+  /usr/bin/psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $UNIT_TEST_DB_USER WITH PASSWORD '$UNIT_TEST_DB_PASSWORD'; ALTER USER $UNIT_TEST_DB_USER CREATEDB;"
+
   /usr/bin/pg_restore -U $POSTGRES_USER -d $POSTGRES_DB $BACKUP_FILE;
+ 
 elif [ -e /docker-entrypoint-initdb.d/db/git.token ]; then
   echo "[!>] Found token, restoring from git"
   GIT_TOKEN=`cat /docker-entrypoint-initdb.d/db/git.token`
@@ -27,9 +29,9 @@ elif [ -e /docker-entrypoint-initdb.d/db/git.token ]; then
       mv $BACKUP_FILE /docker-entrypoint-initdb.d/db/db.backup
       rm -rf /docker-entrypoint-initdb.d/db/backup/
 
-      /usr/bin/pg_restore -U $POSTGRES_USER -d $POSTGRES_DB /docker-entrypoint-initdb.d/db/db.backup
       /usr/bin/psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $UNIT_TEST_DB_USER WITH PASSWORD '$UNIT_TEST_DB_PASSWORD'; ALTER USER $UNIT_TEST_DB_USER CREATEDB;"
 
+      /usr/bin/pg_restore -U $POSTGRES_USER -d $POSTGRES_DB /docker-entrypoint-initdb.d/db/db.backup
     else
       echo "[!>] Cannot restore, failed to find database file after cloning repo"
     fi
