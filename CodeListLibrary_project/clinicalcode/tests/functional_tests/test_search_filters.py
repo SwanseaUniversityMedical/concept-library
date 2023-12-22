76e5492861bc3cfd48024f20aac9e27b227ad664
Arthur <zinnurov_2012@mail.ru>
2023-12-22 15:03:55 +0300

    adding comments and generic entity for future

--- CodeListLibrary_project/clinicalcode/tests/functional_tests/test_search_filters.py
+++ CodeListLibrary_project/clinicalcode/tests/functional_tests/test_search_filters.py
@@ -11,9 +11,11 @@ class TestSearchFilters:
     @pytest.mark.parametrize('user_type', ['super_user'])
     def test_tags_filter(self, login, logout, generate_user, user_type,live_server):
         user = generate_user[user_type]
-        login(self.driver, user.username, user.password)
-        print(live_server.url)
-        self.driver.get(live_server + reverse('search_phenotypes'))    
+        #generate_entity.created_by = generate_user[user_type] this needed to test the page for at least some data
+
+        login(self.driver, user.username, user.username+"password")
+
+        self.driver.get(live_server + reverse('search_phenotypes')) 
 
         accordian = self.driver.find_element(By.XPATH, "/html/body/main/div/div/aside/div[2]/div[4]")
         time.sleep(5)
