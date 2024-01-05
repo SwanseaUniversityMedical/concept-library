94468da0ff282e226c6d3a764f158c95ad58421d
Arthur <zinnurov_2012@mail.ru>
2024-01-05 23:06:06 +0300

    changed logic for the confest file

--- CodeListLibrary_project/clinicalcode/tests/conftest.py
+++ CodeListLibrary_project/clinicalcode/tests/conftest.py
@@ -84,8 +84,10 @@ def generate_entity(create_groups):
 
 @pytest.fixture(scope="class")
 def setup_webdriver(request):
-
-    driver = webdriver.Remote(command_executor=REMOTE_TEST_HOST, options=chrome_options)
+    if REMOTE_TEST:
+        driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',options=chrome_options)
+    else:
+        driver = webdriver.Remote(command_executor=REMOTE_TEST_HOST, options=chrome_options)
     wait = WebDriverWait(driver, 10)
     driver.maximize_window()
     request.cls.driver = driver
