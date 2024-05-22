4384099b0fc24ba55463fe4b91065e84b16278c7
alexcoldea <alexcoldea3@gmail.com>
2024-05-22 10:09:45 +0100

    test auth pheno access functional tests

--- CodeListLibrary_project/clinicalcode/tests/functional_tests/test_auth_user.py
+++ CodeListLibrary_project/clinicalcode/tests/functional_tests/test_auth_user.py
@@ -1,6 +1,5 @@
 import json
 from datetime import datetime
-from pathlib import Path
 
 import pytest
 from django.utils.timezone import make_aware
@@ -9,36 +8,31 @@ from pyconceptlibraryclient import Client
 from clinicalcode.models import GenericEntity
 from clinicalcode.entity_utils.constants import APPROVAL_STATUS, WORLD_ACCESS_PERMISSIONS
 
-# Importing relevant modules and classes
-
 @pytest.mark.django_db
 @pytest.mark.usefixtures("setup_webdriver")
 class TestAuthPhenoAccess:
-
     """
     Test class to verify authentication and user access control for phenotypes.
     """
 
     @pytest.fixture
-    def template_definition(self):
-        """
-        Fixture to load template definition from JSON file.
-        """
-        with open('../constants/test_template.json') as f:
-            new_template = json.load(f)
-        return new_template
-    
-    @pytest.fixture
     def generic_entity(self, create_groups, template):
         """
         Fixture to create a generic entity with specified attributes.
+
+        Args:
+            create_groups (dict): Dictionary of created groups.
+            template (Template): Template object for the entity.
+
+        Returns:
+            GenericEntity: A generic entity with defined attributes.
         """
-        with open('../constants/test_template.json') as f:
+        with open('test_template.json') as f:
             template_definition = json.load(f)
-        template.definition = template_definition 
+        template.definition = template_definition
         template.save()
         
-        generate_entity = GenericEntity(
+        generic_entity = GenericEntity(
             name="Test entity",
             author="Tester author",
             group=create_groups['permitted_group'],
@@ -47,14 +41,18 @@ class TestAuthPhenoAccess:
             publish_status=APPROVAL_STATUS.APPROVED
         )
 
-        generate_entity.world_access = WORLD_ACCESS_PERMISSIONS.NONE
-        return generate_entity
-    
+        return generic_entity
 
     @pytest.mark.parametrize('user_type', ['super_user'])
     def test_user_with_access(self, generate_user, user_type, live_server, generic_entity):
         """
-        Test to verify if a user can access phenotypes with group/world access.
+        Test to verify if a user can access unpublished phenotypes with group/world access.
+
+        Args:
+            generate_user (dict): Dictionary of generated users.
+            user_type (str): Type of user to test.
+            live_server (LiveServer): Live server instance for testing.
+            generic_entity (GenericEntity): The generic entity fixture.
         """
         user = generate_user[user_type]
 
@@ -66,9 +64,6 @@ class TestAuthPhenoAccess:
         generic_entity.owner = user
         generic_entity.created_by = user
         generic_entity.save()
-
-        template_list = client.templates.get()
-        print("Available template list:", template_list)
 
         client.phenotypes.create('../constants/test_create_phenotype_w_access.yaml')
 
@@ -82,13 +77,23 @@ class TestAuthPhenoAccess:
 
         pheno_ver_normal_user = normal_user_client.phenotypes.get_versions('PH2')
         print("Phenotype PH2 with group/world access:", pheno_ver_normal_user)
-        assert pheno_ver_normal_user != []             
-    
-    
+        assert pheno_ver_normal_user != []  
+
+        non_auth_client = Client(public=True, url=live_server.url)
+        non_auth_user = non_auth_client.phenotypes.get_versions('PH2')
+        print("Non authenticated user cannot view unpublished phenotype", non_auth_user)
+        assert non_auth_user == []
+
     @pytest.mark.parametrize('user_type', ['super_user'])
     def test_user_without_access(self, generate_user, user_type, live_server, generic_entity):
         """
         Test to verify if a user can view phenotypes without group/world access.
+
+        Args:
+            generate_user (dict): Dictionary of generated users.
+            user_type (str): Type of user to test.
+            live_server (LiveServer): Live server instance for testing.
+            generic_entity (GenericEntity): The generic entity fixture.
         """
         user = generate_user[user_type]
 
@@ -115,17 +120,44 @@ class TestAuthPhenoAccess:
         )
 
         get_user_match_phenos = normal_user_client.phenotypes.get_versions('PH2')
+        print(get_user_match_phenos)
         print("User can't view phenotype without world/group access:", get_user_match_phenos)
         assert get_user_match_phenos == []
-
 
     def test_non_authenticated_user_view_published_phenos(self):
         """
         Test to verify that non-authenticated users can view published phenotypes.
         """
-        client = Client(public=True)
+        client = Client(public=True, url='http://127.0.0.1:8000/')
 
         get_user_match_phenos = client.phenotypes.get_versions('PH1')
         print(get_user_match_phenos)
         print("User can view published phenotypes:", get_user_match_phenos)
         assert get_user_match_phenos != []
+
+    def test_non_authenticated_user_view_unpublished_phenos(self):
+        """
+        Test to verify that non-authenticated users can view published phenotypes.
+        """
+        client = Client(public=True, url='http://127.0.0.1:8000/')
+
+        get_user_match_phenos = client.phenotypes.get_versions('PH1')
+        print(get_user_match_phenos)
+        print("User can view published phenotypes:", get_user_match_phenos)
+        assert get_user_match_phenos != []
+
+    def test_non_authenticated_user_create_phenos(self):
+        """
+        Test to verify that non-authenticated users can't create phenotypes.
+        """
+        client = Client(public=True, url='http://127.0.0.1:8000/')
+        
+        try:
+            client.phenotypes.create('../constants/test_create_pheno_no_access.yaml')
+        except RuntimeError as e:
+            # Check if the error is 403
+            assert '403' in str(e), \
+                f"Expected 403 or 404 error, but got {str(e)}"
+        else:
+            print("Phenotype could not have been created due to user not being Authenticated")
+            assert False, "Expected RuntimeError for non-authenticated user creating phenotypes, but no error was raised."
