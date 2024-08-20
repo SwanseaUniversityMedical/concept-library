953a0c7c3e63653124d2f5434de3bae3f3073865
Arthur zinnurov <zinnurov_2012@mail.ru>
2024-08-20 12:04:27 +0100

    showing the info more clearly

--- CodeListLibrary_project/cll/static/js/clinicalcode/forms/clinical/endorsementCreator.js
+++ CodeListLibrary_project/cll/static/js/clinicalcode/forms/clinical/endorsementCreator.js
@@ -41,11 +41,14 @@ import {
  *
  */
 const endorsement_ITEM_ELEMENT =
-  '<div class="publication-list-group__list-item" data-target="${index}"> \
-  <div class="publication-list-group__list-item-url"> \
-    <p>${endorsement_organisation} - ${date}</p> \
-  </div> \
-  <button class="publication-list-group__list-item-btn" data-target="${index}"> \
+  '<div class="publication-list-group__list-item" data-target="${index}" style="display: flex; justify-content: space-between; align-items: center;"> \
+  <div class="publication-list-group__list-item-url" style="flex: 1;">\
+    <p style="margin: 0;">${endorsement_organisation}</p> \
+  </div>\
+  <div class="publication-list-group__list-item-date" style="flex: 1; text-align: center;">\
+    <p style="margin: 0;">${date}</p> \
+  </div>\
+  <button class="publication-list-group__list-item-btn" data-target="${index}" style="margin-left: 10px;"> \
     <span class="delete-icon"></span> \
     <span>Remove</span> \
   </button> \
